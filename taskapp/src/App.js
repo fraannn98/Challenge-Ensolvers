c573d1e83518b4ae4b82e79c50e679727544adbc
Franco My <fraannn98@gmail.com>
2022-03-20 00:46:00 -0300

    Front-end structure

--- taskapp/src/App.js
+++ taskapp/src/App.js
@@ -1,33 +1,83 @@
-import React, { useState } from "react";
-import {TaskRow} from './components/TaskRow'
+import React, { useState, useEffect } from "react";
+import { TaskRow } from './components/TaskRow';
+import { TaskBanner } from './components/TaskBanner';
+import { TaskCreator } from './components/TaskCreator';
+import { VisibilityControl } from './components/VisibilityControl';
 
 function App() {
-  const [userName, setUserName] = useState("franco");
+  const [userName, setUserName] = useState("Franco");
   const [taskItems, setTaskItems] = useState([
-    { name: "Task one", done: false },
-  ])
+  ]);
+  const [showCompleted, setShowCompleted] = useState(true);
+
+  useEffect(() => {
+    let data = localStorage.getItem("tasks");
+    if (data != null) {
+      setTaskItems(JSON.parse(data))
+    } else {
+        setUserName("Franco");
+        setTaskItems([
+       ]);
+      setShowCompleted(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("task", JSON.stringify(taskItems));
+  }, [taskItems]);
+
+  const createNewTask = taskName => {
+    if (!taskItems.find(t => t.name === taskName)) {
+      setTaskItems([...taskItems, { name: taskName, done: false }]);
+    }
+  };
 
   const toggleTask = task =>
-    setTaskItems(taskItems.map(t => (t.name === task.name ? {...t, done: !t.done} : t)))
+    setTaskItems(
+      taskItems.map(t => (t.name === task.name ? { ...t, done: !t.done } : t))
+    );
 
-  const taskTableRows = () => 
-    taskItems.map(task => (  
-      <TaskRow task={task} key={task.name} toggleTask={toggleTask} />
-    ))
+  const taskTableRows = doneValue => 
+    taskItems
+      .filter(task => task.done === doneValue)
+      .map(task => (
+        <TaskRow key={task.name} task={task} toggleTask={toggleTask} />
+      ));
   
 
   return (
-    <div className="App">
-      <h1>hellow world</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Description</th>
-            <th>Done</th>
-          </tr>
-        </thead>
-        <body>{taskTableRows()}</body>
-      </table>
+    <div>
+      <TaskBanner userName={userName} taskItems={taskItems} />
+      <div className="container-fluid">
+        <TaskCreator callback={createNewTask} />
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>{taskTableRows(false)}</tbody>
+        </table>
+        <div className="bg-secondary text-white text-center p-2">
+          <VisibilityControl
+            description="Completed Task"
+            isChecked={showCompleted}
+            callback={checked => setShowCompleted(checked)}
+          />  
+        </div>
+        {showCompleted && (
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Description</th>
+                <th>Done</th>
+              </tr>
+            </thead>
+            <tbody>{taskTableRows(true)}</tbody>
+          </table>
+        )}  
+      </div>
     </div>
   );
 }
